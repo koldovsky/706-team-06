80bfccf91341d5262e501f508f4c489d3d77001d
MaryanaSeniv <marianaseniv1398@gmail.com>
2022-07-29 00:30:05 +0300

    channged classes to pricing

--- js/pricing.js
+++ js/pricing.js
@@ -1,8 +1,8 @@
 (function () {
-  const countDiscountTimer = new Date("Jul 31, 2022 23:59:59").getTime();
-  const discountTimer = setInterval(function () {
+  const discountEnd = new Date("Jul 31, 2022 23:59:59").getTime();
+  const countDiscountTimer = setInterval(function () {
     const now = new Date().getTime();
-    const timeleft = countDiscountTimer - now;
+    const timeleft = discountEnd - now;
     const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -14,12 +14,12 @@
     document.querySelector('.minutes__discount-left').innerHTML = minutes + "m ";
     document.querySelector('.seconds__discount-left').innerHTML = seconds + "s ";
     if (timeleft < 0) {
-      clearInterval(discountTimer);
+      clearInterval(countDiscountTimer);
       document.querySelector('.days__discount-left').innerHTML = "";
       document.querySelector('.hours__discount-left').innerHTML = "";
       document.querySelector('.minutes__discount-left').innerHTML = "";
       document.querySelector('.seconds__discount-left').innerHTML = "";
-      document.querySelector('.discount-left').innerHTML = "NEXT TIME!";
+      document.querySelector('.discount__left').innerHTML = "NEXT TIME!";
     }
   }, 1000);
 })();
