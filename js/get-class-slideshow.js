12b052da2a75fa80f68b7532cbb3ddf8e082f944
panfilenkooleh <panfilenkooleh@gmail.com>
2022-07-31 15:05:16 +0300

    css upd js change

--- js/get-class-slideshow.js
+++ js/get-class-slideshow.js
@@ -1,22 +1,24 @@
 
-// (function () {
+(function () {
 
-let currentIndex = 1;
-displaySlides(currentIndex);
+  let slideIndex = 0;
+  showSlides();
 
-function setSlides(num) {
-displaySlides(currentIndex += num);
-}
-
-function displaySlides(num) {
-  let i;
-  const slides = document.getElementsByClassName("getclass-imageSlides");
-  if (num > slides.length) { currentIndex = 1 }
-  if (num < 1) { currentIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("getclass-imageSlides");
+    let dots = document.getElementsByClassName("getclass-dot");
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000);
   }
-  slides[currentIndex - 1].style.display = "block";
-}
 
-// })();
+})();
