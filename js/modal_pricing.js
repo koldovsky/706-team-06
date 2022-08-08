94120aaff71ac1386eeb368ebe68e2fab5a57d91
MaryanaSeniv <marianaseniv1398@gmail.com>
2022-08-08 21:40:15 +0300

    make open modal_pricing with different buttons

--- js/modal_pricing.js
+++ js/modal_pricing.js
@@ -1,11 +1,17 @@
 (function () {
   const modalPricing = document.querySelector(".modal__window-pricing");
-  const openPricing = document.querySelector(".select__plan");
-  const closePricing = document.querySelector(".close__window-pricing");
-  openPricing.onclick = function () {
+  const openPricing = document.getElementsByClassName("select__plan");
+  const closePricing = document.getElementsByClassName("close__window-pricing");
+  for (let i=0; i < openPricing.length; i++) {
+    openPricing[i].addEventListener("click", openModal);
+  }
+  function openModal ()  {
     modalPricing.style.display = "block";
   };
-  closePricing.onclick = function () {
+  for (let x=0; x < closePricing.length; x++) {
+    closePricing[x].addEventListener("click", closeModal);
+  }
+  function closeModal () {
     modalPricing.style.display = "none";
   };
   window.onclick = function (event) {
