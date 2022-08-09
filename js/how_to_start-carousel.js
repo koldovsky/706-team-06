a1d712cb9db8f966017166bc8c898e59a9743848
aaaandriana <aaaandriana.07@gmail.com>
2022-08-09 11:20:27 +0000

    delate buttons

--- js/how_to_start-carousel.js
+++ js/how_to_start-carousel.js
@@ -22,22 +22,5 @@
         start__slideContainer.innerHTML = start_slides[currentStartSlide];
         
     }
-    function prevStart() {
-        currentStartSlide = currentStartSlide - 1 < 0 ? start_slides.length - 1 : currentStartSlide + 1;
-        renderHowToStartCarousel();
-    }
-
-    function nextStart() {
-        currentStartSlide = currentStartSlide + 1 >= start_slides.length ? 0 : currentStartSlide + 1;
-        renderHowToStartCarousel();
-    }
-
-
-const prevButton = document.querySelector('.start__slideshow__btn-prev');
-prevButton.addEventListener('click', prev);
-const nextButton = document.querySelector('.start__slideshow__btn-next');
-nextButton.addEventListener('click', next);
-
-
 
 })();
